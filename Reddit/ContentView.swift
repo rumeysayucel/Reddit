59c6ef1f7f92e91d531c9defb84c2060f8b64b92
Rumeysa Yücel <rumeysa.yucel.1996@gmail.com>
2022-02-05 13:30:26 +0300

    Initial Commit

---
+++ Reddit/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Reddit
+//
+//  Created by Rumeysa Yücel on 5.02.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Reddit/RedditApp.swift
@@ -0,0 +1,17 @@
+//
+//  RedditApp.swift
+//  Reddit
+//
+//  Created by Rumeysa Yücel on 5.02.2022.
+//
+
+import SwiftUI
+
+@main
+struct RedditApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
